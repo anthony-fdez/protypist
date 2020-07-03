2bfb38516a23d5f8edb2a8a37479d57adc3b2a6e
Anthony Fdez <anthonyfdez23@gmail.com>
2020-07-03 19:57:49 -0400

    d

--- src/components/TypingTest/TypingTest.js
+++ src/components/TypingTest/TypingTest.js
@@ -11,13 +11,15 @@ function TypingTest() {
   );
   const [textArrayCharacters, setTextArrayCharacters] = useState();
   const [infoAboutCharacter, setInfoAboutCharacter] = useState();
+  const [charactersTyped, setCharactersTyped] = useState();
+  const [spanArray, setSpanArray] = useState();
 
   useEffect(() => {
     const splitedText = text.split("");
 
     let infoAboutCharacterObject = [];
     splitedText.map((character, index) => {
-      let object = "null";
+      let object = null;
       infoAboutCharacterObject.push(object);
     });
 
@@ -25,22 +27,49 @@ function TypingTest() {
     setInfoAboutCharacter(infoAboutCharacterObject);
   }, []);
 
+  useEffect(() => {
+    setSpanArray(displayTheArray());
+  }, [charactersTyped, textArrayCharacters]);
+
   const displayTheArray = () => {
     if (textArrayCharacters !== undefined) {
       const newArray = textArrayCharacters.map((character, index) => {
-        return (
-          <span className={`span-${index}`} key={index}>
-            {character}
-          </span>
-        );
+        if (textArrayCharacters[charactersTyped] === true) {
+          return (
+            <span className="green" key={index}>
+              {character}
+            </span>
+          );
+        } else if (textArrayCharacters[charactersTyped] === false) {
+          return (
+            <span className="red" key={index}>
+              {character}
+            </span>
+          );
+        } else
+          return (
+            <span className="none" key={index}>
+              {character}
+            </span>
+          );
       });
       return newArray;
     }
   };
 
   const getAndCheckTheInput = (e) => {
+    setCharactersTyped(e.target.value.length);
     if (textArrayCharacters !== undefined) {
       if (
+        infoAboutCharacter[e.target.value.length] === false ||
+        infoAboutCharacter[e.target.value.length] === true
+      ) {
+        let array = [...infoAboutCharacter];
+        let arrayItem = array[e.target.value.length - 1];
+        arrayItem = null;
+        array[e.target.value.length] = arrayItem;
+        setInfoAboutCharacter(array);
+      } else if (
         e.target.value[e.target.value.length - 1] ===
         textArrayCharacters[e.target.value.length - 1]
       ) {
@@ -49,7 +78,6 @@ function TypingTest() {
         arrayItem = true;
         array[e.target.value.length - 1] = arrayItem;
         setInfoAboutCharacter(array);
-        console.log(array);
       } else if (
         e.target.value[e.target.value.length - 1] !==
         textArrayCharacters[e.target.value.length - 1]
@@ -59,9 +87,16 @@ function TypingTest() {
         arrayItem = false;
         array[e.target.value.length - 1] = arrayItem;
         setInfoAboutCharacter(array);
-        console.log(array);
       }
     }
+  };
+
+  const checkForTheClass = () => {
+    if (infoAboutCharacter[charactersTyped - 1] === true) {
+      return "green";
+    } else if (infoAboutCharacter[charactersTyped - 1] === false) {
+      return "red";
+    } else return "standard";
   };
 
   return (
@@ -69,12 +104,14 @@ function TypingTest() {
       <div className="TypingTest">
         <Header text="Improve your typing skills" />
         <div className="text-to-type">
-          <h2>{displayTheArray()}</h2>
+          <h2>{spanArray}</h2>
         </div>
         <div className="input-zone">
           <h2>Type the text above </h2>
           <input
-            onChange={getAndCheckTheInput}
+            onChange={(e) => {
+              getAndCheckTheInput(e);
+            }}
             placeholder="The test will bigin when you start typing!"
             className="input-box"
           ></input>
