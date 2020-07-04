edd89ec02c046b5f70616fffeecae3f5b943ca16
Anthony Fdez <anthonyfdez23@gmail.com>
2020-07-04 16:57:51 -0400

    a

--- src/components/TypingTest/TypingTest.js
+++ src/components/TypingTest/TypingTest.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./TypingTest.css";
+
 import Header from "../header/header";
+
 import { useSelector } from "react-redux";
 import { useSpring, animated } from "react-spring";
 
@@ -8,12 +10,23 @@ function TypingTest() {
   const theme = useSelector((state) => state.darkModeReducer);
   //state
   const [text, setText] = useState(
-    "Once upon a time there was a car that was so fast that no other car never even came closw to such high speeds, thats it."
+    "This is just a text that I am typing just to test if my game is working, this is some more text to make sure the algorithem is working properly"
   );
   const [textArrayCharacters, setTextArrayCharacters] = useState();
   const [infoAboutCharacter, setInfoAboutCharacter] = useState();
-  const [charactersTyped, setCharactersTyped] = useState();
+  const [charactersTyped, setCharactersTyped] = useState(0);
   const [spanArray, setSpanArray] = useState();
+  const [blankInfoArray, setBlankInfoArray] = useState([]);
+
+  //-----------------------------------------------
+  const [isRunning, setIsRunning] = useState(false);
+  const [timeSeconds, setTimeSeconds] = useState(0);
+  const [newGame, setNewGame] = useState(false);
+  const [blankSpanArray, setBlankSpanArray] = useState([]);
+  const [mistakes, setMistakes] = useState(0);
+  const [wpm, setWPM] = useState(0);
+  const [cpm, setCPM] = useState(0);
+  const [isUserTyping, setIsUserTyping] = useState(true);
 
   useEffect(() => {
     const splitedText = text.split("");
@@ -24,13 +37,26 @@ function TypingTest() {
       infoAboutCharacterObject.push(object);
     });
 
+    setBlankInfoArray(infoAboutCharacterObject);
     setTextArrayCharacters(splitedText);
     setInfoAboutCharacter(infoAboutCharacterObject);
   }, []);
 
   useEffect(() => {
-    setSpanArray(displayTheArray());
-  }, [charactersTyped, textArrayCharacters]);
+    if (newGame === true) {
+      setSpanArray(blankSpanArray);
+    } else setSpanArray(displayTheArray());
+  }, [charactersTyped, textArrayCharacters, newGame]);
+
+  useEffect(() => {
+    if (textArrayCharacters !== undefined) {
+      let spanArray = [];
+      for (let i = 0; i < textArrayCharacters.length; i++) {
+        spanArray.push(<div className="none">{textArrayCharacters[i]}</div>);
+      }
+      setInfoAboutCharacter(spanArray);
+    }
+  }, [newGame]);
 
   const displayTheArray = () => {
     if (textArrayCharacters !== undefined) {
@@ -52,7 +78,66 @@ function TypingTest() {
     }
   };
 
+  //This is to check for errors
+  useEffect(() => {
+    if (infoAboutCharacter !== undefined) {
+      let errors = 0;
+      for (let i = 0; i < infoAboutCharacter.length; i++) {
+        if (infoAboutCharacter[i] === false) {
+          errors++;
+        }
+      }
+
+      setMistakes(errors);
+    }
+  }, [charactersTyped]);
+
+  useEffect(() => {
+    setNewGame(false);
+  }, [newGame]);
+
+  //========== words per minute ================
+
+  const calculateWordsPerMinute = () => {
+    let charactersPerSecond = charactersTyped / timeSeconds;
+    let wordsPerMinute = (charactersPerSecond * 60) / 5;
+    let charactersPerMinute = charactersPerSecond * 60;
+
+    wordsPerMinute = Math.round(wordsPerMinute);
+    charactersPerMinute = Math.round(charactersPerMinute);
+
+    setCPM(charactersPerMinute);
+    setWPM(wordsPerMinute);
+  };
+
   const getAndCheckTheInput = (e) => {
+    if (
+      e.target.value.length === textArrayCharacters.length &&
+      mistakes === 0
+    ) {
+      calculateWordsPerMinute();
+      setTimeSeconds(0);
+      e.target.value = "";
+      setIsRunning(false);
+      setNewGame(true);
+      setSpanArray(blankSpanArray);
+      setInfoAboutCharacter(blankInfoArray);
+    } else if (charactersTyped >= 1) {
+      setIsRunning(true);
+    }
+
+    let inputArray = e.target.value.split(" ");
+    for (let i = 0; i < inputArray.length; i++) {
+      if (inputArray[i] === "//restart") {
+        e.target.value = "";
+        setIsRunning(false);
+        setNewGame(true);
+        setSpanArray(blankSpanArray);
+        setInfoAboutCharacter(blankInfoArray);
+        setMistakes(0);
+      }
+    }
+
     setCharactersTyped(e.target.value.length);
     if (textArrayCharacters !== undefined) {
       if (
@@ -86,10 +171,34 @@ function TypingTest() {
     }
   };
 
+  useEffect(() => {
+    if (isRunning) {
+      let interval = setInterval(() => {
+        setTimeSeconds((seconds) => seconds + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    } else if (isRunning === false) {
+      let time = timeSeconds;
+      setTimeSeconds(time);
+    }
+  }, [isRunning]);
+
   const animation = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
   });
+
+  const changeTextToTypeClassname = () => {
+    if (theme) {
+      if (isUserTyping) {
+        return "text-to-type";
+      } else return "text-to-type-dark";
+    } else {
+      if (isUserTyping) {
+        return "text-to-type";
+      } else return "text-to-type-light";
+    }
+  };
 
   return (
     <animated.div
@@ -98,20 +207,44 @@ function TypingTest() {
     >
       <div className="TypingTest">
         <Header text="Improve your typing skills" />
-        <div className="text-to-type">{spanArray}</div>
-        <p className="alert-primary alert">
-          <strong>Type the text above</strong>, start typing whenever you are
-          ready :)
+        <div className="statistics">
+          <h3>WPM:{wpm}</h3>
+          <h3>Characters per minute:{cpm}</h3>
+          <h3>Errors:{mistakes}</h3>
+        </div>
+        <hr className={theme ? "white-hr" : "dark-hr"}></hr>
+        <p
+          className={
+            isRunning
+              ? "alert-primary alert-hidden"
+              : "alert-primary alert-shown"
+          }
+        >
+          {isUserTyping
+            ? "Start typing... Start to type the text below whenever you are ready :)"
+            : "Click on the text to start typing."}
         </p>
+        <div className={changeTextToTypeClassname()}>{spanArray}</div>
+
         <div className="input-zone">
           <input
             autoFocus
+            onFocus={(e) => {
+              setIsUserTyping(true);
+            }}
+            onBlur={(e) => {
+              setIsUserTyping(false);
+            }}
             onChange={(e) => {
               getAndCheckTheInput(e);
             }}
             placeholder="The test will bigin when you start typing!"
             className="input-box form-control"
           ></input>
+          <p className="alert-warning alert-tip">
+            <strong>Tip:</strong> you can type "//restart" any time you want to
+            restart the test.
+          </p>
         </div>
       </div>
     </animated.div>
