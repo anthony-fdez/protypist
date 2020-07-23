19e2d2d8f92012ec76c4ef58f8e2f8c9e0174408
Anthony <anthony@pop-os.localdomain>
2020-07-23 16:47:46 -0400

    Added the 10 second game

--- src/components/10second/10second.js
+++ src/components/10second/10second.js
@@ -1,30 +1,198 @@
-import React from "react";
-import "./10second.css";
-import Header from "../header/header";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "react-spring";
+import { Link } from "react-router-dom";
 
-function Typing10Second() {
-  const theme = useSelector((state) => state.darkModeReducer);
+import Input from "./input";
+import "./10second.css";
 
-  const animation = useSpring({
+function Game10seconds() {
+  const [darkMode] = useState(true);
+  const [isMenuInScreen, setIsMenuInScreen] = useState(false);
+  const [dificulty, setDificulty] = useState(1); // easy: 2seconds, normal: 1second, harn 0.4seconds
+  const [language, setLanguage] = useState(true); // TRUE is English, FALSE is spanish
+
+  /*================== Getting the data from the browser ==================*/
+
+  useEffect(() => {
+    let userLanguage = window.localStorage.getItem("language");
+    if (userLanguage === null) {
+      setLanguage(true);
+      window.localStorage.setItem("language", "true");
+    } else if (userLanguage === "true") {
+      setLanguage(true);
+    } else if (userLanguage === "false") {
+      setLanguage(false);
+    }
+  }, []);
+
+  // Seting the settings to the browser
+
+  useEffect(() => {
+    //window.localStorage.setItem("mode", darkMode);
+    window.localStorage.setItem("language", language);
+  }, [darkMode, language]);
+
+  /*================== Fading in animation ==================*/
+
+  const props = useSpring({
+    config: { duration: 1000 },
     from: { opacity: 0 },
     to: { opacity: 1 },
-    config: { duration: 300 },
   });
 
-  return (
-    <animated.div
-      style={animation}
-      className={
-        theme ? "Typing10Second-page-dark" : "Typing10Second-page-light"
-      }
-    >
-      <div className={"Typing10Second"}>
-        <Header text="10second Game" />
+  const changeDificultyButtonColors = (isDeficultyEqual) => {
+    if (dificulty === isDeficultyEqual) {
+      return "dificulty-button btn btn-primary m-1";
+    } else return "dificulty-button btn btn-light m-1";
+  };
+
+  const enableClickingOutsideToCloseTheMenu = () => {
+    if (isMenuInScreen) {
+      setIsMenuInScreen(false);
+    }
+  };
+
+  /*================== Sliding Menu ==================*/
+
+  function slidingMenu() {
+    return (
+      <div
+        className={
+          isMenuInScreen ? "ten-second-menu-open" : "ten-second-menu-closed"
+        }
+      >
+        <div className="ten-second-sliding-menu-div">
+          <div className="how-to-play">
+            <h3 style={{ color: "rgb(0, 118, 253)", "text-align": "center" }}>
+              How to pay?
+            </h3>
+            <hr></hr>
+            <p>
+              Easy... Just type the word that is avobe where you type, every
+              time you get one word right it'll add time depending on the
+              dificulty, easy adds 2seconds, normal adds 1 and hard 0.5. When
+              you run out of time you lose.{" "}
+            </p>
+            <p></p>
+          </div>
+          <div className="settings-page-info">
+            <div className="sliding-menu-top-settings">
+              <h1>{language ? "Settings" : "Ajustes"}</h1>
+            </div>
+            <hr className={"hr-white"}></hr>
+            <div className={darkMode ? "settings-dark" : "settings-light"}>
+              <h5 className="m-0">{language ? "Language:" : "Idioma:"}</h5>
+              <button
+                onClick={() => {
+                  setLanguage(!language);
+                }}
+                className={"btn btn-light"}
+                // className={darkMode ? "btn btn-light" : "btn btn-dark"}
+              >
+                {language ? "Eng" : "Esp"}
+              </button>
+            </div>
+            <div
+              className={
+                darkMode ? "settings-dificult-dark" : "settings-dificult-light"
+              }
+            >
+              <div>
+                <h5>{language ? "Dificulty:" : "Dificultad:"}</h5>
+              </div>
+              <div className="dificulty-buttons">
+                <button
+                  onClick={() => {
+                    setDificulty(2);
+                  }}
+                  className={changeDificultyButtonColors(2)}
+                >
+                  {language ? "Easy" : "Facil"}
+                </button>
+                <button
+                  onClick={() => {
+                    setDificulty(1);
+                  }}
+                  className={changeDificultyButtonColors(1)}
+                >
+                  {language ? "Normal" : "Normal"}
+                </button>
+                <button
+                  onClick={() => {
+                    setDificulty(0.5);
+                  }}
+                  className={changeDificultyButtonColors(0.5)}
+                >
+                  {language ? "Hard" : "Dificil"}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            onClick={enableClickingOutsideToCloseTheMenu}
+            className="close-botton"
+          >
+            x
+          </div>
+        </div>
       </div>
-    </animated.div>
+    );
+  }
+
+  return (
+    <div className="typing-10-second">
+      <div
+        className={isMenuInScreen ? "dark-mode-open" : "dark-mode-closed"}
+        onClick={isMenuInScreen ? enableClickingOutsideToCloseTheMenu : null}
+      >
+        {/* <Div100vh className={darkMode ? "dark-mode" : "light-mode"}></Div100vh> */}
+        <animated.div style={props}>
+          <div>
+            <div>
+              <div className={"App transition  p-3 container"}>
+                <div className="title ">
+                  <h1>{language ? "10 second chalenge!" : "Escribidor!"}</h1>
+                  <h5 style={{ color: "rgb(0, 118, 253)" }}>
+                    Type fast, you only have 10 seconds
+                  </h5>
+                </div>
+              </div>
+              <div
+                onClick={() => {
+                  setIsMenuInScreen(false);
+                }}
+              >
+                <div className="container mt-3">
+                  <Input
+                    dificulty={dificulty}
+                    language={language}
+                    darkMode={darkMode}
+                  />
+                </div>
+              </div>
+
+              <div className="settings-div container">
+                <div>
+                  <Link to="/">
+                    <div className="nav-bottons">
+                      <h5>Back to Menu</h5>
+                    </div>
+                  </Link>
+                </div>
+                <div>
+                  <div
+                    onClick={() => setIsMenuInScreen(!isMenuInScreen)}
+                    className="nav-bottons"
+                  >
+                    <h5>Settings</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </animated.div>
+      </div>
+    </div>
   );
 }
-
-export default Typing10Second;
+export default Game10seconds;
---
+++ src/components/10second/input.js
@@ -0,0 +1,292 @@
+import React, { useState, useEffect } from "react";
+import Typical from "react-typical";
+
+import "./input.css";
+
+//Add more words and work on making the words not reapeat!
+
+function Input(props) {
+  const [lost, setLost] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
+  const [randomWord, setRandomWord] = useState("");
+  const [seconds, setSeconds] = useState(9);
+  const [miliseconds, setMiliseconds] = useState(0);
+  const [score, setScore] = useState(0);
+  const [addSecond, setAddSecond] = useState(false);
+  const [highestScore, setHighestScore] = useState(0);
+  const [countingUpSeconds, setCountingUpSeconds] = useState(0); //the counting up seconds are to calculate the words per minut
+  const [wpm, setWPM] = useState(0);
+  const [keyPressed, setKeyPressed] = useState(0);
+
+  const [words] = useState(() => {
+    let randomNewWords = require("random-words");
+    return randomNewWords(2000); //2000 words is probably too much but this almost makes the words not repeat
+  });
+
+  /*================== Select the random words ==================*/
+
+  useEffect(() => {
+    if (props.language === false) {
+      const seletRandomWord = () => {
+        let json = require("./palabras.json");
+        let random = Math.floor(
+          Math.random() * Math.floor(json.palabras.length)
+        );
+        let randomWord = json.palabras[random];
+        return randomWord;
+      };
+      setRandomWord(seletRandomWord());
+    } else {
+      const seletRandomWord = () => {
+        let random = Math.floor(Math.random() * Math.floor(words.length));
+
+        let randomWord = words[random];
+        return randomWord;
+      };
+      setRandomWord(seletRandomWord());
+    }
+  }, [score, lost, words, props.language]);
+
+  /*================== Clock countdown ==================*/
+
+  useEffect(() => {
+    let intervalSeconds = null;
+    let intervalMiliseconds = null;
+
+    if (isRunning) {
+      intervalMiliseconds = setInterval(() => {
+        setMiliseconds((miliseconds) => miliseconds - 1);
+      }, 100);
+    } else if (seconds === 0) {
+      setIsRunning(false);
+      setLost(true);
+    }
+    if (miliseconds > 10) {
+      setMiliseconds((miliseconds) => miliseconds - 10);
+      setSeconds((seconds) => seconds + 1);
+    }
+    if (miliseconds <= 0) {
+      setMiliseconds(9);
+      setSeconds((seconds) => seconds - 1);
+    }
+
+    if (seconds < 0) {
+      setLost(true);
+      setIsRunning(false);
+    }
+    return () => {
+      clearInterval(intervalSeconds);
+      clearInterval(intervalMiliseconds);
+    };
+  }, [seconds, isRunning, miliseconds]);
+
+  /*================== Clock counting up ==================*/
+
+  useEffect(() => {
+    let intervalSeconds = null;
+
+    if (isRunning) {
+      intervalSeconds = setInterval(() => {
+        setCountingUpSeconds((countingUpSeconds) => countingUpSeconds + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(intervalSeconds);
+    };
+  }, [countingUpSeconds, isRunning]);
+
+  /*================== Calculate words per minute ==================*/
+
+  useEffect(() => {
+    const calculateWordPerMinute = () => {
+      if (isRunning) {
+        let wordsPerMinute = (score / countingUpSeconds) * 60;
+        setWPM(wordsPerMinute);
+      }
+      if (score > highestScore) {
+        setHighestScore(score);
+      }
+      if (isRunning === false && lost === true) {
+        setCountingUpSeconds(0);
+      }
+    };
+    calculateWordPerMinute();
+  }, [score, isRunning, keyPressed]);
+
+  /*================== Select the text to display at the befining of the game ==================*/
+
+  const startOfTheGame = () => {
+    if (isRunning) {
+      return (
+        <div className="time-and-word-to-type container d-flex justify-content-between">
+          <h3 className="word-to-type">{randomWord.split("").join(" ")}</h3>
+
+          <div className="">
+            <h5
+              className={
+                props.darkMode
+                  ? "time-left text-light "
+                  : "time-left text-dark "
+              }
+            >
+              {seconds}:{miliseconds < 10 ? `${miliseconds}0` : miliseconds}
+            </h5>
+          </div>
+        </div>
+      );
+    } else if (isRunning === false && lost === true) {
+      return (
+        <div>
+          <h1 className="start-message">
+            <Typical
+              className="word-to-type"
+              wraper="b"
+              steps={["Game over."]}
+            />
+          </h1>
+        </div>
+      );
+    } else if (isRunning === false) {
+      return (
+        <div className="game-over-message">
+          <Typical
+            className="word-to-type"
+            wraper="b"
+            steps={['Type "start" to begin!']}
+          />
+        </div>
+      );
+    }
+  };
+
+  /*================== Show the +1 second  ==================*/
+
+  const displayAddingSeconds = () => {
+    if (addSecond) {
+      return (
+        <h5 className="add-second text-success">
+          {`+${props.dificulty}`}
+          {props.language ? " second" : " segundo"}
+        </h5>
+      );
+    }
+  };
+
+  /*================== Change the place holder of the input box ==================*/
+
+  const placeholderChange = () => {
+    let placeholder = "";
+    if (isRunning && props.language) {
+      placeholder = `Type: "${randomWord}"`;
+    } else if (isRunning && props.language === false) {
+      placeholder = `Escribe "${randomWord}"`;
+    } else if (!isRunning && props.language) {
+      placeholder = 'Type "start" to begin';
+    } else if (!isRunning && !props.language) {
+      placeholder = 'Escribe "vamo a darle" para empezar a jugar!';
+    }
+    return placeholder;
+  };
+
+  /*================== Check for equal word ==================*/
+
+  const checkForEqualWord = (e) => {
+    setKeyPressed((keyPressed) => {
+      return keyPressed + 1;
+    });
+    if (isRunning) {
+      if (e.target.value === randomWord) {
+        e.target.value = "";
+        setScore((score) => score + 1);
+        if (props.dificulty === 1 || props.dificulty === 2) {
+          setSeconds((seconds) => seconds + props.dificulty);
+        } else if (props.dificulty === 0.5) {
+          setMiliseconds((miliseconds) => miliseconds + 5);
+        }
+      }
+    } else if (isRunning === false) {
+      if (e.target.value === "start" || e.target.value === "vamo a darle") {
+        setSeconds(9);
+        setMiliseconds(9);
+        setLost(false);
+        setIsRunning(true);
+        setScore(0);
+        setWPM(0);
+        e.target.value = "";
+      }
+    }
+  };
+
+  /*================== This is what makes the "1+ second" text only show up for half a second ==================*/
+
+  useEffect(() => {
+    const resolveAfter500ms = () => {
+      return new Promise((resolve) => {
+        setAddSecond(true);
+        setTimeout(() => {
+          resolve(setAddSecond(false));
+        }, 500);
+      });
+    };
+    const asyncCall = async () => {
+      await resolveAfter500ms();
+    };
+    asyncCall();
+  }, [score]);
+
+  /*================== Render section ==================*/
+
+  return (
+    <div className="input">
+      <div>{startOfTheGame()}</div>
+      <div className="input">
+        <input
+          autoFocus
+          spellCheck="false"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          onChange={checkForEqualWord}
+          placeholder={placeholderChange()}
+          type="text"
+          className={
+            seconds < 3 && lost === false
+              ? "transition form-control form-running-out-of-time"
+              : " transition form-control form-input"
+          }
+        />
+      </div>
+      <div className="mt-3 ml-2 d-flex justify-content-between">
+        <h4
+          className={
+            props.darkMode ? "transition text-light" : "transition text-dark"
+          }
+        >
+          {props.language ? "Score: " : "Puntuacion: "}{" "}
+          {addSecond ? "+1" : score}
+        </h4>
+        <div>{displayAddingSeconds()}</div>
+      </div>
+      <div>
+        <h5
+          className={
+            props.darkMode ? "highest-score-dark" : "highest-score-light"
+          }
+        >
+          {props.language ? "Highest Score: " : "Puntuacion ma's alta: "}
+          {highestScore}
+        </h5>
+        <h5 //this is the words per minute h5, it says highest score but it doesn't matter
+          className={
+            props.darkMode ? "highest-score-dark" : "highest-score-light"
+          }
+        >
+          {props.language ? "WPM: " : "Palabras por minuto: "}
+          {wpm === NaN ? "0" : wpm.toFixed(1)}
+        </h5>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
