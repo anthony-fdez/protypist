0b4ea0044606e3802fdce4ffd35f5848d34e9c05
Anthony Fdez <fernndez.anthony@yahoo.com>
2020-10-01 15:09:19 -0400

    added a couple of things

--- src/components/customText/selectText.js
+++ src/components/customText/selectText.js
@@ -27,6 +27,39 @@ const SelectText = () => {
         className="TypingTest"
       >
         <Header />
+        <div
+          style={{ backgroundColor: colorFiles.secondaryBackgroundColor }}
+          className="select-text-div"
+        >
+          <h4>Type of paste the text you want to type in the box below!</h4>
+          <hr style={{ background: colorFiles.hrColor }}></hr>
+          <div
+            contentEditable="true"
+            style={{
+              backgroundColor: colorFiles.secondSecondaryBackgroundColor,
+              color: colorFiles.fontColor,
+            }}
+            className="select-text-input"
+          >
+            Type your text here!
+          </div>
+          <div className="select-text-info-div">
+            <div className="select-text-info">
+              <h5 style={{ marginRight: "3rem" }}>Words:</h5>
+              <h5>Characters:</h5>
+            </div>
+            <button
+              style={{
+                backgroundColor: colorFiles.primaryColor,
+                color: colorFiles.fontColor,
+                fontSize: "16px",
+              }}
+              className="btn btn-light start-button"
+            >
+              Start typing
+            </button>
+          </div>
+        </div>
       </div>
     </animated.div>
   );
