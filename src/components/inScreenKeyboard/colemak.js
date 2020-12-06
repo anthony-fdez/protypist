892c2600f45c3bd7e4437d629c309c7bd9964125
Anthony Fdez <anthonyfdez23@gmail.com>
2020-12-06 00:06:09 -0500

    Updated

--- src/components/inScreenKeyboard/colemak.js
+++ src/components/inScreenKeyboard/colemak.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./keyboard.css";
 
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function useKeyPress(targetKey) {
   const [keyPressed, setKeyPressed] = useState(false);
@@ -35,6 +35,47 @@ function useKeyPress(targetKey) {
 }
 
 function Colemak() {
+  const dispatch = useDispatch();
+
+  const [pressed_A_Count, setPressed_A_Count] = useState(0);
+  const [pressed_B_Count, setPressed_B_Count] = useState(0);
+  const [pressed_C_Count, setPressed_C_Count] = useState(0);
+  const [pressed_D_Count, setPressed_D_Count] = useState(0);
+  const [pressed_E_Count, setPressed_E_Count] = useState(0);
+  const [pressed_F_Count, setPressed_F_Count] = useState(0);
+  const [pressed_G_Count, setPressed_G_Count] = useState(0);
+  const [pressed_H_Count, setPressed_H_Count] = useState(0);
+  const [pressed_I_Count, setPressed_I_Count] = useState(0);
+  const [pressed_J_Count, setPressed_J_Count] = useState(0);
+  const [pressed_K_Count, setPressed_K_Count] = useState(0);
+  const [pressed_L_Count, setPressed_L_Count] = useState(0);
+  const [pressed_M_Count, setPressed_M_Count] = useState(0);
+  const [pressed_N_Count, setPressed_N_Count] = useState(0);
+  const [pressed_O_Count, setPressed_O_Count] = useState(0);
+  const [pressed_P_Count, setPressed_P_Count] = useState(0);
+  const [pressed_Q_Count, setPressed_Q_Count] = useState(0);
+  const [pressed_R_Count, setPressed_R_Count] = useState(0);
+  const [pressed_S_Count, setPressed_S_Count] = useState(0);
+  const [pressed_T_Count, setPressed_T_Count] = useState(0);
+  const [pressed_U_Count, setPressed_U_Count] = useState(0);
+  const [pressed_V_Count, setPressed_V_Count] = useState(0);
+  const [pressed_W_Count, setPressed_W_Count] = useState(0);
+  const [pressed_X_Count, setPressed_X_Count] = useState(0);
+  const [pressed_Y_Count, setPressed_Y_Count] = useState(0);
+  const [pressed_Z_Count, setPressed_Z_Count] = useState(0);
+  const [pressed_1_Count, setPressed_1_Count] = useState(0);
+  const [pressed_2_Count, setPressed_2_Count] = useState(0);
+  const [pressed_3_Count, setPressed_3_Count] = useState(0);
+  const [pressed_4_Count, setPressed_4_Count] = useState(0);
+  const [pressed_5_Count, setPressed_5_Count] = useState(0);
+  const [pressed_6_Count, setPressed_6_Count] = useState(0);
+  const [pressed_7_Count, setPressed_7_Count] = useState(0);
+  const [pressed_8_Count, setPressed_8_Count] = useState(0);
+  const [pressed_9_Count, setPressed_9_Count] = useState(0);
+  const [pressed_0_Count, setPressed_0_Count] = useState(0);
+  const [pressed_Space_Count, setPressed_Space_Count] = useState(0);
+  const [pressed_Shift_Count, setPressed_Shift_Count] = useState(0);
+
   const colors = useSelector((state) => state.themeReducer);
   const colorFiles = require(`../themes/${colors}`);
   const pressedA = useKeyPress("a");
@@ -134,6 +175,197 @@ function Colemak() {
   const pressedQuestion = useKeyPress("?");
   const pressedBackSpace = useKeyPress("Backspace");
   const pressedShift = useKeyPress("Shift");
+
+  useEffect(() => {
+    if (pressedA || pressedCapitalA) {
+      setPressed_A_Count((pressed) => (pressed = pressed_A_Count + 1));
+    } else if (pressedB || pressedCapitalB) {
+      setPressed_B_Count((pressed) => (pressed = pressed_B_Count + 1));
+    } else if (pressedC || pressedCapitalC) {
+      setPressed_C_Count((pressed) => (pressed = pressed_C_Count + 1));
+    } else if (pressedD || pressedCapitalD) {
+      setPressed_D_Count((pressed) => (pressed = pressed_D_Count + 1));
+    } else if (pressedE || pressedCapitalE) {
+      setPressed_E_Count((pressed) => (pressed = pressed_E_Count + 1));
+    } else if (pressedF || pressedCapitalF) {
+      setPressed_F_Count((pressed) => (pressed = pressed_F_Count + 1));
+    } else if (pressedG || pressedCapitalG) {
+      setPressed_G_Count((pressed) => (pressed = pressed_G_Count + 1));
+    } else if (pressedH || pressedCapitalH) {
+      setPressed_H_Count((pressed) => (pressed = pressed_H_Count + 1));
+    } else if (pressedI || pressedCapitalI) {
+      setPressed_I_Count((pressed) => (pressed = pressed_I_Count + 1));
+    } else if (pressedJ || pressedCapitalJ) {
+      setPressed_J_Count((pressed) => (pressed = pressed_J_Count + 1));
+    } else if (pressedK || pressedCapitalK) {
+      setPressed_K_Count((pressed) => (pressed = pressed_K_Count + 1));
+    } else if (pressedL || pressedCapitalL) {
+      setPressed_L_Count((pressed) => (pressed = pressed_L_Count + 1));
+    } else if (pressedM || pressedCapitalM) {
+      setPressed_M_Count((pressed) => (pressed = pressed_M_Count + 1));
+    } else if (pressedN || pressedCapitalN) {
+      setPressed_N_Count((pressed) => (pressed = pressed_N_Count + 1));
+    } else if (pressedO || pressedCapitalO) {
+      setPressed_O_Count((pressed) => (pressed = pressed_O_Count + 1));
+    } else if (pressedP || pressedCapitalP) {
+      setPressed_P_Count((pressed) => (pressed = pressed_P_Count + 1));
+    } else if (pressedQ || pressedCapitalQ) {
+      setPressed_Q_Count((pressed) => (pressed = pressed_Q_Count + 1));
+    } else if (pressedR || pressedCapitalR) {
+      setPressed_R_Count((pressed) => (pressed = pressed_R_Count + 1));
+    } else if (pressedS || pressedCapitalS) {
+      setPressed_S_Count((pressed) => (pressed = pressed_S_Count + 1));
+    } else if (pressedT || pressedCapitalT) {
+      setPressed_T_Count((pressed) => (pressed = pressed_T_Count + 1));
+    } else if (pressedU || pressedCapitalU) {
+      setPressed_U_Count((pressed) => (pressed = pressed_U_Count + 1));
+    } else if (pressedV || pressedCapitalV) {
+      setPressed_V_Count((pressed) => (pressed = pressed_V_Count + 1));
+    } else if (pressedW || pressedCapitalW) {
+      setPressed_W_Count((pressed) => (pressed = pressed_W_Count + 1));
+    } else if (pressedX || pressedCapitalX) {
+      setPressed_X_Count((pressed) => (pressed = pressed_X_Count + 1));
+    } else if (pressedY || pressedCapitalY) {
+      setPressed_Y_Count((pressed) => (pressed = pressed_Y_Count + 1));
+    } else if (pressedZ || pressedCapitalZ) {
+      setPressed_Z_Count((pressed) => (pressed = pressed_Z_Count + 1));
+    } else if (pressed1) {
+      setPressed_1_Count((pressed) => (pressed = pressed_1_Count + 1));
+    } else if (pressed2) {
+      setPressed_2_Count((pressed) => (pressed = pressed_2_Count + 1));
+    } else if (pressed3) {
+      setPressed_3_Count((pressed) => (pressed = pressed_3_Count + 1));
+    } else if (pressed4) {
+      setPressed_4_Count((pressed) => (pressed = pressed_4_Count + 1));
+    } else if (pressed5) {
+      setPressed_5_Count((pressed) => (pressed = pressed_5_Count + 1));
+    } else if (pressed6) {
+      setPressed_6_Count((pressed) => (pressed = pressed_6_Count + 1));
+    } else if (pressed7) {
+      setPressed_7_Count((pressed) => (pressed = pressed_7_Count + 1));
+    } else if (pressed8) {
+      setPressed_8_Count((pressed) => (pressed = pressed_8_Count + 1));
+    } else if (pressed9) {
+      setPressed_9_Count((pressed) => (pressed = pressed_9_Count + 1));
+    } else if (pressed0) {
+      setPressed_0_Count((pressed) => (pressed = pressed_0_Count + 1));
+    } else if (pressedShift) {
+      setPressed_Shift_Count((pressed) => (pressed = pressed_Shift_Count + 1));
+    } else if (pressedSpace) {
+      setPressed_Space_Count((pressed) => (pressed = pressed_Space_Count + 1));
+    }
+  }, [
+    pressedA,
+    pressedCapitalA,
+    pressedB,
+    pressedCapitalB,
+    pressedC,
+    pressedCapitalC,
+    pressedD,
+    pressedCapitalD,
+    pressedE,
+    pressedCapitalE,
+    pressedF,
+    pressedCapitalF,
+    pressedG,
+    pressedCapitalG,
+    pressedH,
+    pressedCapitalH,
+    pressedI,
+    pressedCapitalI,
+    pressedJ,
+    pressedCapitalJ,
+    pressedK,
+    pressedCapitalK,
+    pressedL,
+    pressedCapitalL,
+    pressedM,
+    pressedCapitalM,
+    pressedN,
+    pressedCapitalN,
+    pressedO,
+    pressedCapitalO,
+    pressedP,
+    pressedCapitalP,
+    pressedQ,
+    pressedCapitalQ,
+    pressedR,
+    pressedCapitalR,
+    pressedS,
+    pressedCapitalS,
+    pressedT,
+    pressedCapitalT,
+    pressedU,
+    pressedCapitalU,
+    pressedV,
+    pressedCapitalV,
+    pressedW,
+    pressedCapitalW,
+    pressedX,
+    pressedCapitalX,
+    pressedY,
+    pressedCapitalY,
+    pressedZ,
+    pressedCapitalZ,
+    pressed1,
+    pressed2,
+    pressed3,
+    pressed4,
+    pressed5,
+    pressed6,
+    pressed7,
+    pressed8,
+    pressed9,
+    pressed0,
+    pressedShift,
+    pressedSpace,
+  ]);
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_KEYS_PRESSED",
+      payload: {
+        a: pressed_A_Count,
+        b: pressed_B_Count,
+        c: pressed_C_Count,
+        d: pressed_D_Count,
+        e: pressed_E_Count,
+        f: pressed_F_Count,
+        g: pressed_G_Count,
+        h: pressed_H_Count,
+        i: pressed_I_Count,
+        j: pressed_J_Count,
+        k: pressed_K_Count,
+        l: pressed_L_Count,
+        m: pressed_M_Count,
+        n: pressed_N_Count,
+        o: pressed_O_Count,
+        p: pressed_P_Count,
+        q: pressed_Q_Count,
+        r: pressed_R_Count,
+        s: pressed_S_Count,
+        t: pressed_T_Count,
+        u: pressed_U_Count,
+        v: pressed_V_Count,
+        w: pressed_W_Count,
+        x: pressed_X_Count,
+        y: pressed_Y_Count,
+        z: pressed_Z_Count,
+        ONE: pressed_1_Count,
+        TWO: pressed_2_Count,
+        THREE: pressed_3_Count,
+        FOUR: pressed_4_Count,
+        FIVE: pressed_5_Count,
+        SIX: pressed_6_Count,
+        SEVEN: pressed_7_Count,
+        EIGHT: pressed_8_Count,
+        NINE: pressed_9_Count,
+        ZERO: pressed_0_Count,
+        Space: pressed_Space_Count,
+        Shift: pressed_Shift_Count,
+      },
+    });
+  }, [pressedSpace]);
 
   return (
     <div>
