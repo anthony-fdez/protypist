13617d59467229fcd178deb3febf02a4b596fb16
Anthony Fdez <anthonyfdez23@gmail.com>
2020-12-11 19:13:46 -0500

    Added the buttons to sort the leaderboard

--- src/components/10second/10secondsLeaderboard.js
+++ src/components/10second/10secondsLeaderboard.js
@@ -10,22 +10,67 @@ const TenSecondsLeaderboard = (props) => {
   const isLoggedIn = useSelector((state) => state.isLoggedInReducer);
   const jwt = useSelector((state) => state.JWTreducer);
   const [leaderboardData, setLeaderboardData] = useState(null);
+  const [difficulty, setDifficulty] = useState("NORMAL");
 
   useEffect(() => {
     if (isLoggedIn) {
       const headers = { Authorization: jwt };
 
-      Axios.get("https://protypist.herokuapp.com/10seconds/leaderboard/easy", {
-        headers: headers,
-      })
-        .then((response) => {
-          setLeaderboardData(response.data.leaderboard_easy);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      if (difficulty === "EASY") {
+        Axios.get(
+          "https://protypist.herokuapp.com/10seconds/leaderboard/easy",
+          {
+            headers: headers,
+          }
+        )
+          .then((response) => {
+            setLeaderboardData(response.data.leaderboard_easy);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else if (difficulty === "NORMAL") {
+        Axios.get(
+          "https://protypist.herokuapp.com/10seconds/leaderboard/normal",
+          {
+            headers: headers,
+          }
+        )
+          .then((response) => {
+            setLeaderboardData(response.data.leaderboard_normal);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else if (difficulty === "HARD") {
+        Axios.get(
+          "https://protypist.herokuapp.com/10seconds/leaderboard/hard",
+          {
+            headers: headers,
+          }
+        )
+          .then((response) => {
+            setLeaderboardData(response.data.leaderboard_hard);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      } else if (difficulty === "EPIC") {
+        Axios.get(
+          "https://protypist.herokuapp.com/10seconds/leaderboard/epic",
+          {
+            headers: headers,
+          }
+        )
+          .then((response) => {
+            setLeaderboardData(response.data.leaderboard_epic);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
     }
-  }, []);
+  }, [difficulty]);
 
   return (
     <div
@@ -47,6 +92,112 @@ const TenSecondsLeaderboard = (props) => {
         ></i>
       </div>
       <hr style={{ background: colorFiles.hrColor }}></hr>
+      <div style={{ textAlign: "center" }}>
+        <h3>Rank by mode</h3>
+        <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+          <button
+            style={
+              difficulty === "EASY"
+                ? {
+                    backgroundColor: colorFiles.primaryColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+                : {
+                    backgroundColor: colorFiles.backgroundColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+            }
+            className="btn btn-light select-difficulty-button"
+            onClick={() => setDifficulty("EASY")}
+          >
+            Easy
+          </button>
+          <button
+            style={
+              difficulty === "NORMAL"
+                ? {
+                    backgroundColor: colorFiles.primaryColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+                : {
+                    backgroundColor: colorFiles.backgroundColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+            }
+            className="btn btn-light select-difficulty-button"
+            onClick={() => setDifficulty("NORMAL")}
+          >
+            Normal
+          </button>
+          <button
+            style={
+              difficulty === "HARD"
+                ? {
+                    backgroundColor: colorFiles.primaryColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+                : {
+                    backgroundColor: colorFiles.backgroundColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+            }
+            className="btn btn-light select-difficulty-button"
+            onClick={() => setDifficulty("HARD")}
+          >
+            Hard
+          </button>
+          <button
+            style={
+              difficulty === "EPIC"
+                ? {
+                    backgroundColor: colorFiles.primaryColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+                : {
+                    backgroundColor: colorFiles.backgroundColor,
+                    color: colorFiles.fontColor,
+                    marginRight: "5px",
+                    marginLeft: "5px",
+                    width: "80px",
+                    border: "none",
+                  }
+            }
+            className="btn btn-light select-difficulty-button"
+            onClick={() => setDifficulty("EPIC")}
+          >
+            Epic
+          </button>
+        </div>
+      </div>
+      <hr style={{ background: colorFiles.hrColor, width: "80%" }}></hr>
       <div>
         <div style={{ position: "relative", width: "80%", margin: "auto" }}>
           <div className="test-history-item">
